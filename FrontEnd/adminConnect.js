d701616fe582f929327a0d2499619325207d25bf
Serge Kouadio Bi <kouadiobiserge@gmail.com>
2023-05-20 06:24:24 +0200

    version
    fini

--- FrontEnd/adminConnect.js
+++
@@ -1,8 +0,0 @@
-const token = sessionStorage.getItem('token');
-
-if (token) {
-	const welcomeMessage = document.getElementById('welcome-message');
-	welcomeMessage.textContent = 'Bienvenue ! Vous êtes maintenant authentifié.';
-} else {
-	window.location.href = 'login.html';
-}
